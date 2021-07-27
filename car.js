e1e9f52a6300c04d11039d7aca5af9082c68b4b2
hd2342-dubey <79002024+hd2342-dubey@users.noreply.github.com>
2021-07-27 19:47:42 +0530

    Add files via upload

---
+++ car.js
@@ -0,0 +1,10 @@
+var audio = document.createElement('audio');
+audio.setAttribute('src', 'car_sound.mp3');
+audio.loop = true;
+audio.play();
+
+// var audio = new Audio('car_sound.mp3');
+// audio.play();
+
+// const audio = new Audio("audio.mp3");
+
